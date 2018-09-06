c78d2fbd806edcd642ef1206cd73b00f1df94b3b
alexliu16 <alexliu@Alexs-MacBook-Air.local>
2018-09-06 08:00:24 -0700

    Support escape character

--- Chapter3/src/wci/frontend/java/tokens/JavaCharacterToken.java
+++ Chapter3/src/wci/frontend/java/tokens/JavaCharacterToken.java
@@ -31,14 +31,24 @@ public class JavaCharacterToken extends JavaToken {
 	protected void extract() throws Exception {
 		StringBuilder textBuffer = new StringBuilder();
 		StringBuilder valueBuffer = new StringBuilder();
+		
+		boolean escapeChar = false; 
 
 		char currentChar = nextChar(); // consume initial quote
 		textBuffer.append('\'');
 		valueBuffer.append('\'');
 		
-		if(currentChar != '\'' && currentChar != '\\') {
+		if(currentChar != '\'') {
 			textBuffer.append(currentChar);
 			valueBuffer.append(currentChar);
+			
+			if(currentChar == '\\') {
+				currentChar = nextChar();
+				textBuffer.append(currentChar);
+				valueBuffer.append(currentChar);
+				escapeChar = true;
+			}
+
 			currentChar = nextChar();
 		}
 		else { 
@@ -55,7 +65,12 @@ public class JavaCharacterToken extends JavaToken {
 			
 			type = CHARACTER;
 			value = valueBuffer.toString();
-		} else {
+		}
+		else if(escapeChar) {
+			type = ERROR;
+			value = INVALID_CHARACTER;
+		}
+		else {
 			type = ERROR;
 			value = UNEXPECTED_EOF;
 		}
