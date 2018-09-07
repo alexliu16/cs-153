aab6e35e5665ccff68a07dcc25f6b64d4f46b7ef
alexliu16 <alexliu@Alexs-MacBook-Air.local>
2018-09-07 15:40:39 -0700

    Fix character token value

--- Chapter3/TestFile.java
+++ Chapter3/TestFile.java
@@ -1,25 +1,25 @@
-//Testing Special symbols, numbers and comments.
-/*some other line 2342*/3
-432 54.34 3.3e3 3e3 2.0 3. 2//comment at end of line
-< <= > >= != == = >> << >>= <<= |= &= ^= ~= ++ -- += -= /= *= && || % ~ ^ & * ( ) { } [ ] - + . , / ? : @
-/* This is a multi-line comment.
-It spans more than one line
- */ 2-2*2.3e3 321^%
-
- // Word tokens
- abstract double break else case enum char extends class float const
- for continue goto do if int long native return short package protected
- static super switch super this throw void volatile while
- Hello world
- Abstract abstract ABSTRACT aBsTrAcT What?
- ___Ident2312313ifier____ 123beepbeep
-
- // String tokens
- "Hello, world."
- "Hello,\tworld!"
- "Hello,\n\"world!\""
- "It's Friday!"
-"  "
-"__ye__"
-
- 'x' 'A' '\'' 'a' '\n' '\t' '\\'
+/* This is a comment. */
+// So is this.
+/* Here's a comment
+ that spans several
+ source lines. */
+Two/*comments in*//***a row***/ here.
+/* This is /* not a nested comment. */
+// Nor is /* this */ one.
+{ Not a comment. }
+// Word tokens
+Hello world
+Abstract abstract ABSTRACT aBsTrAcT
+What?
+// Character tokens
+'x' 'A' '\'' 'a' '\n' '\t' '\\'
+// String tokens
+"Hello, world."
+"Hello,\tworld!"
+"Hello,\n\"world!\""
+"It's Friday!"
+"" "\""
+// Special symbol tokens
++ - * / := . , ; : = <> < <= >= > ( ) [ ] { } } ^ ..
+<<= >>=
+:=<>=<==>>===
